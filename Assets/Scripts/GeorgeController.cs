c49a108ec8a4366703f29390e2270135212b9ab9
concubicycle <saveliyb@gmail.com>
2020-07-10 18:45:23 -0700

    floor and wall tilesets, with wall collisions.

--- Assets/Scripts/GeorgeController.cs
+++ Assets/Scripts/GeorgeController.cs
@@ -9,12 +9,14 @@ public class GeorgeController : MonoBehaviour
 
     private Animator _animator;
     private Transform _transform;
+    private Rigidbody2D _rb;
 
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
         _transform = GetComponent<Transform>();
+        _rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -25,28 +27,29 @@ public class GeorgeController : MonoBehaviour
         if (Input.GetKey("w"))
         {
             _animator.Play("Base Layer.George_Front");
-            _transform.position += new Vector3(0, Speed * Time.deltaTime, 0);
+            _rb.velocity = new Vector3(0, Speed, 0);
         }
         else if (Input.GetKey("s"))
         {
             _animator.Play("Base Layer.George_Back");
-            _transform.position -= new Vector3(0, Speed * Time.deltaTime, 0);
+            _rb.velocity = new Vector3(0, -Speed, 0);
         }
         else if (Input.GetKey("d"))
         {
             _animator.Play("Base Layer.George_Right");
-            _transform.position += new Vector3(Speed * Time.deltaTime, 0, 0);
+            _rb.velocity = new Vector3(Speed, 0, 0);
         }
         else if (Input.GetKey("a"))
         {
             _animator.Play("Base Layer.George_Left");
-            _transform.position -= new Vector3(Speed * Time.deltaTime, 0, 0);
+            _rb.velocity = new Vector3(-Speed, 0, 0);
         }
         else
         {
             var state = _animator.GetCurrentAnimatorStateInfo(0);            
             _animator.Play(state.nameHash, 0, 0);
             _animator.speed = 0;
+            _rb.velocity = Vector3.zero;
         }
     }
 }
