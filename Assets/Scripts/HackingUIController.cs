f59ddb53b5a791622a4b23f94b6bc57877ff7d58
Lauri Johannes Leponiemi <lauri.leponiemi@cs.tamk.fi>
2020-07-12 00:07:38 +0300

    Buttons can be set for as many robots as you want. Do note you have to give both the robot a reference to the button, and the button a reference to the robot.

---
+++ Assets/Scripts/HackingUIController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HackingUIController : MonoBehaviour
+{
+
+    HackingUI currentButton;
+
+    private void SetButton(HackingUI cB)
+    {
+        currentButton = cB;
+    }
+
+    private void NullButton()
+    {
+        currentButton = null;
+    }
+
+    public bool IsButtonActive()
+    {
+        return currentButton != null;
+    }
+
+    public bool IsThisActive(HackingUI tB)
+    {
+        return tB == currentButton;
+    }
+}
