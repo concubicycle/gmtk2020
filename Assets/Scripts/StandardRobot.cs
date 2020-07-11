dd5eb927eae17883664892379f2caf6b8427201f
concubicycle <saveliyb@gmail.com>
2020-07-10 20:55:06 -0700

    Taking over robot with WASD

--- Assets/Scripts/StandardRobot.cs
+++ Assets/Scripts/StandardRobot.cs
@@ -1,4 +1,5 @@
 ﻿using FMOD;
+using Pathfinding;
 using System;
 using System.Collections;
 using UnityEngine;
@@ -9,18 +10,27 @@ namespace Assets.Scripts
     {
         Routine,
         PlayerControlled,
-        Insane,
-        Returning
+        Insane,        
+        Broken
     }
 
     class StandardRobot : MonoBehaviour
     {
         public RobotState State = RobotState.Routine;
+        public float InputTimeout = 0.5f;
 
         private Coroutine _currentRoutine = null;
 
+        private AIPath _aiPath;
+        private Rigidbody2D _rigidbody;
+        private Animator _animator;
+
         private void Start()
         {
+            _aiPath = GetComponent<AIPath>();
+            _rigidbody = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
+
             TransitionTo(State);
         }
 
@@ -44,24 +54,73 @@ namespace Assets.Scripts
                 case RobotState.PlayerControlled:
                     StartCoroutine(DoPlayerControlled());
                     break;
-                case RobotState.Returning:
-                    StartCoroutine(DoReturning());
-                    break;
             }
         }
 
         private IEnumerator DoRoutine()
         {
+            _aiPath.enabled = true;
+            
             while (State == RobotState.Routine)
             {
+                if (Input.GetKey("w") ||
+                     (Input.GetKey("s")) ||
+                     (Input.GetKey("d")) ||
+                     (Input.GetKey("a")))
+                {
+                    TransitionTo(RobotState.PlayerControlled);
+                }
+
                 yield return 0;
             }
         }
 
         private IEnumerator DoPlayerControlled()
         {
+            float inputTimeoutRemaining = InputTimeout;
+            _aiPath.enabled = false;
+
+
             while (State == RobotState.PlayerControlled)
             {
+                if (Input.GetKey("w"))
+                {
+                    _animator.Play("Base Layer.Robot_Up");
+                    _rigidbody.velocity = new Vector3(0, _aiPath.maxSpeed, 0);
+                    inputTimeoutRemaining = InputTimeout;
+                }
+                else if (Input.GetKey("s"))
+                {
+                    _animator.Play("Base Layer.Robot_Down");
+                    _rigidbody.velocity = new Vector3(0, -_aiPath.maxSpeed, 0);
+                    inputTimeoutRemaining = InputTimeout;
+                }
+                else if (Input.GetKey("d"))
+                {
+                    _animator.Play("Base Layer.Robot_Right");
+                    _rigidbody.velocity = new Vector3(_aiPath.maxSpeed, 0, 0);
+                    inputTimeoutRemaining = InputTimeout;
+                }
+                else if (Input.GetKey("a"))
+                {
+                    _animator.Play("Base Layer.Robot_Left");
+                    _rigidbody.velocity = new Vector3(-_aiPath.maxSpeed, 0, 0);
+                    inputTimeoutRemaining = InputTimeout;
+                }
+                else
+                {
+                    var state = _animator.GetCurrentAnimatorStateInfo(0);
+                    _animator.Play(state.nameHash, 0, 0);
+                    _animator.speed = 0;
+                    _rigidbody.velocity = Vector3.zero;
+                    inputTimeoutRemaining -= Time.deltaTime;
+                }
+
+                if (inputTimeoutRemaining <= 0)
+                {
+                    TransitionTo(RobotState.Routine);
+                }
+
                 yield return 0;
             }
         }
@@ -74,9 +133,9 @@ namespace Assets.Scripts
             }
         }
 
-        private IEnumerator DoReturning()
+        private IEnumerator DoBroken()
         {
-            while (State == RobotState.Returning)
+            while (State == RobotState.Broken)
             {
                 yield return 0;
             }
