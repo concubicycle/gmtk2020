528802d542a320924604f82eb9ee3e1766039f80
Kristinn Bragi <kristinnbragi@gmail.com>
2020-07-11 22:22:41 +0200

    Added script for soundtrack parameters

---
+++ Assets/Scripts/Audio/Music.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class Music : MonoBehaviour
+{
+    FMOD.Studio.EventInstance MusicEvent;
+    private Sanity Sanity;
+    private float Intensity = 0f;
+    private int currentLevel = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Sanity = GetComponent<Sanity> ();
+        MusicEvent = FMODUnity.RuntimeManager.CreateInstance("event:/Music/Level 1");
+        MusicEvent.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MusicEvent.setParameterByName("Intensity", Intensity);
+        MusicEvent.setParameterByName("Level", currentLevel);
+    }
+}
