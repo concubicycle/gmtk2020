4f2e69af039078d7d51a10dd3e47c266c536c29b
concubicycle <saveliyb@gmail.com>
2020-07-12 07:13:06 -0700

    scientist

--- Assets/Scripts/HumanScientist.cs
+++ Assets/Scripts/HumanScientist.cs
@@ -15,6 +15,8 @@ namespace Assets.Scripts
 
     class HumanScientist : MonoBehaviour
     {
+        public const int RobotLayer = 9;
+
         public string UpAnimation = "Scientist_Up";
         public string RightAnimation = "Scientist_Right";
         public string DownAnimation = "Scientist_Down";
@@ -22,6 +24,10 @@ namespace Assets.Scripts
 
         public ScientistBehavior State = ScientistBehavior.Patrolling;
         public float PatrolStopWaitTime = 1.5f;
+        public int SightRayCount = 15;
+        public float SightConeRadius = Mathf.PI / 4.0f;
+        public float VisibleInsanityCutoff = 80;
+
 
         private Patrol _patrol;
         private AIDestinationSetter _dest;
@@ -29,6 +35,8 @@ namespace Assets.Scripts
         private AIPath _aiPath;
         private GameObject _targetRobot = null;
         private Coroutine _currentRoutine = null;
+
+        private Vector2 _forward;
 
         private void Start()
         {
@@ -56,9 +64,15 @@ namespace Assets.Scripts
             {
                 case ScientistBehavior.Patrolling:
                     _aiPath.enabled = true;
+                    _patrol.enabled = true;
+                    _dest.enabled = false;
                     _currentRoutine = StartCoroutine(DoPatrol());
                     break;
                 case ScientistBehavior.Chasing:
+                    _patrol.enabled = false;
+                    _dest.enabled = true;
+                    _dest.target = _targetRobot.GetComponent<Transform>();
+                    _currentRoutine = StartCoroutine(DoChasing());
                     break;
                 case ScientistBehavior.Recycling:
                     break;
@@ -89,16 +103,63 @@ namespace Assets.Scripts
                     timeSinceDestination += Time.deltaTime;
                 }
 
-                // detect enemies here
+                _forward = (_aiPath.destination - _aiPath.position).normalized;
+
+                DetectMadRobots();
+
+                yield return new WaitForSeconds(0.5f);
+            }
+        }
+
+        private IEnumerator DoChasing()
+        {
+            var caughtDistanceSq = 1;
+
+            while (State == ScientistBehavior.Chasing)
+            {
+                var targetTransform = _targetRobot.GetComponent<Transform>();
+                var transform = GetComponent<Transform>();
+
+                var toTarget = targetTransform.position - transform.position;
+
+                if (toTarget.sqrMagnitude < caughtDistanceSq)
+                {
+                    // pick up robot, carry to trash
+                }
 
                 yield return 0;
             }
         }
 
+        private void DetectMadRobots()
+        {
+            float theta = 0;
 
+            // forward ray
+            // Cast a ray straight down.
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, _forward);
+            CheckHit(hit);
+            
+        }
 
+        private bool CheckHit(RaycastHit2D hit)
+        {
+            // If it hits something...
+            if (hit.collider != null && hit.collider.gameObject.layer == RobotLayer)
+            {
+                var sanity = hit.collider.gameObject.GetComponent<Sanity>();
+                if (sanity.SanityPoints < VisibleInsanityCutoff)
+                {
+                    _targetRobot = hit.collider.gameObject;
+                    TransitionTo(ScientistBehavior.Chasing);
+                }
+                
 
+                return true;
+            }
 
+            return false;
+        }
 
         private void SetAnimatorState(Vector2 velocity)
         {
