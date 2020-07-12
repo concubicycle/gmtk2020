dacef682cea460962099b005b19491f66c954163
Kristinn Bragi <kristinnbragi@gmail.com>
2020-07-12 11:41:29 +0200

    Implemented UI Select SFX for Buttons

---
+++ Assets/Scripts/Audio/UISelectSFX.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts;
+
+public class UISelectSFX : MonoBehaviour
+{
+    FMOD.Studio.EventInstance UISFXEvent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void OnClick()
+    {
+        UISFXEvent = FMODUnity.RuntimeManager.CreateInstance("event:/UI/UI Select");
+        UISFXEvent.start();
+    }
+}
