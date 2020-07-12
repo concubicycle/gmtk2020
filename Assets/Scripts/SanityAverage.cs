6cd4df1367b978de6d98e1718b8f3d62862ec6fa
concubicycle <saveliyb@gmail.com>
2020-07-12 07:25:39 -0700

    sanity avg

---
+++ Assets/Scripts/SanityAverage.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class SanityAverage : MonoBehaviour
+    {
+        public float AverageSanity;
+
+
+        private void Update()
+        {
+            var all = FindObjectsOfType<Sanity>();
+            var n = all.Count();
+            var sum = 0.0f;
+
+            foreach (var obj in all)
+                sum += obj.GetComponent<Sanity>().SanityPoints;
+
+            AverageSanity = sum / n;
+        }
+    }
+}
